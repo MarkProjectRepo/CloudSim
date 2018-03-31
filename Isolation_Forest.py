94568e3c975ee25cd8d33db422373ced178b93bc
markprojectrepo <marktraquair@gmail.com>
2018-03-31 16:22:57 -0400

    Added dotted line outline

--- Isolation_Forest.py
+++ Isolation_Forest.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 
 from sklearn.ensemble import IsolationForest
+from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
@@ -12,23 +13,23 @@ rng = np.random.RandomState(42)
 X = pd.read_csv('Output.csv')#Read in our data
 
 #Normalize the data, but this may not be necessary as scales matter in this case
-#X = StandardScaler().fit_transform(X[['Actual CPU', 'Length','Input Size', 'Output Size']])
+X = StandardScaler().fit_transform(X[['Actual CPU', 'Length','Input Size', 'Output Size']])
 
 #Perform Principle Component Analysis
 pca = PCA(n_components=2)
 X = pca.fit_transform(X) # Reduce dimensionality
 
 #Initialize and fit the forest to the data
-isof = IsolationForest(random_state=rng).fit(X)
+isof = IsolationForest().fit(X)
 
 #Set the range of the graph
-xx, yy = np.meshgrid(np.linspace(-40000, 60000, 500), np.linspace(-50000, 50000, 500))
+xx, yy = np.meshgrid(np.linspace(-5, 5, 50), np.linspace(-5, 5, 50))
 Z = isof.decision_function(np.c_[xx.ravel(), yy.ravel()])
 Z = Z.reshape(xx.shape)#Finish initializing the contour function of the isolation forest
 
 plt.contourf(xx, yy, Z, cmap=plt.cm.Blues_r)#Plot the contour function
-
-b1 = plt.scatter(X[:, 0], X[:, 1], c='yellow', s=5, alpha=0.02)#Plot the data on top of the contour
+plt.contour(xx, yy, Z, linewidths=2, colors='red')
+b1 = plt.scatter(X[:, 0], X[:, 1], c='yellow', s=5, alpha=0.09)#Plot the data on top of the contour
 
 plt.title("Isolation Forest")
 
